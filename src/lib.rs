e27eecbb20fd26b2efeaa21ebb998db47521a73e
Vlad Frolov <frolvlad@gmail.com>
2022-02-18 15:14:03 +0200

    feat: Delay fetching the tip of the network to avoid burning the network with unnecessary requests (#8)
    * feat: Delay fetching the tip of the network to avoid burning the network with unnecessary requests (wip)
    
    * Finish up what @frol has started
    
    * Update src/lib.rs
    
    Co-authored-by: Vlad Frolov <frolvlad@gmail.com>
    
    Co-authored-by: Bohdan Khorolets <bogdan@khorolets.com>

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@ async fn start(
     streamer_message_sink: mpsc::Sender<near_indexer_primitives::StreamerMessage>,
     s3_bucket_name: String,
     s3_region_name: String,
-    index_from_block_height: u64,
+    index_from_block_height: types::BlockHeight,
 ) {
     // instantiate AWS S3 Client
     let region_provider = RegionProviderChain::first_try(Some(s3_region_name).map(Region::new))
@@ -38,37 +38,30 @@ async fn start(
     let shared_config = aws_config::from_env().region(region_provider).load().await;
     let s3_client = Client::new(&shared_config);
 
-    let mut start_from_block_height: String = index_from_block_height.to_string();
+    let mut start_from_block_height = index_from_block_height;
 
     // Continuously get the list of block data from S3 and send them to the `streamer_message_sink`
     loop {
         // TODO: decide what to do if we got an error
         if let Ok(block_heights_prefixes) =
-            s3_fetchers::list_blocks(&s3_client, &s3_bucket_name, start_from_block_height.clone())
-                .await
+            s3_fetchers::list_blocks(&s3_client, &s3_bucket_name, start_from_block_height).await
         {
             // update start_after key
-            start_from_block_height = {
-                if block_heights_prefixes.is_empty() {
-                    start_from_block_height
-                } else {
-                    block_heights_prefixes[block_heights_prefixes.len() - 1]
-                        .split('/')
-                        .collect::<Vec<&str>>()
-                        .get(0)
-                        .map(|s| (s.parse::<u64>().unwrap() + 1).to_string())
-                        .unwrap_or_else(|| start_from_block_height)
-                }
-            };
+            if let Some(last_block_height) = block_heights_prefixes.last() {
+                start_from_block_height = *last_block_height;
+            } else {
+                tokio::time::sleep(std::time::Duration::from_millis(200)).await;
+                continue;
+            }
 
             let mut streamer_messages_futures: futures::stream::FuturesOrdered<_> =
                 block_heights_prefixes
                     .iter()
-                    .map(|block_height_prefix| {
+                    .map(|block_height| {
                         s3_fetchers::fetch_streamer_message(
                             &s3_client,
                             &s3_bucket_name,
-                            block_height_prefix,
+                            *block_height,
                         )
                     })
                     .collect();
