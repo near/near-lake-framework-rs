8422b85b62909b7ed874b7cc40750106280a33d9
Bohdan Khorolets <bogdan@khorolets.com>
2023-06-06 12:15:02 +0300

    docs: Lake Primitives documentation added (#75)
    * feat: ParentTransactionCache context and example
    
    * Update documentation, add docs about the LakeContext
    
    * docs: Lake Primitives documentation added

--- lake-primitives/src/types/block.rs
+++ lake-primitives/src/types/block.rs
@@ -7,6 +7,22 @@ use super::state_changes;
 use super::transactions;
 use crate::near_indexer_primitives::{types::AccountId, views, CryptoHash, StreamerMessage};
 
+/// A structure that represents an entire block in the NEAR blockchain.
+/// It is a high-level structure that is built on top of the low-level [StreamerMessage] structure.
+///
+/// The access to all the data is provided through the getters. Thus we can refactor the structure yet keep the API stable and backward compatible.
+///
+/// With a high-level update we are trying to replace the usage of the low-level [StreamerMessage] with this one.
+///
+/// #### Important notes on the Block
+/// - All the entities located on different shards were merged into one single list without differentiation.
+///   The statement from **NEAR** is that **sharding is going to be dynamic and seamless for the users**, that’s why we’ve decided indexer
+///   developers don’t want to care about shards either.
+/// - Original [near_indexer_primitives::StreamerMessage] represents the blockchain data in *a most fair manner**. Although, it used to be
+///   a pain in the neck for indexer developers, we’ve decided to act as a painkiller here.
+/// - [Block] is not the fairest name for this structure either. **NEAR Protocol** is a sharded blockchain, so its block is actually an
+///   ephemeral structure that represents a collection of *real blocks* called Chunks in **NEAR Protocol**. We’ve been simplifying things here though,
+///   so here is a result of the simplification.
 #[derive(Debug)]
 pub struct Block {
     streamer_message: StreamerMessage,
@@ -19,26 +35,46 @@ pub struct Block {
 }
 
 impl Block {
+    /// Return a reference to the original StreamerMessage of the block. This is the low-level structure.
+    ///
+    /// While introducing the high-level structures, methods, and helpers, we do want to keep the low-level “door” open
+    /// for advanced developers or edge cases which we haven’t accidentally covered, or just don’t have the capacity to cover.
+    ///
+    /// That’s why every instance of the Block will hold the original StreamerMessage for developers.
+    /// Think of it as backward compatibility if you prefer.
     pub fn streamer_message(&self) -> &StreamerMessage {
         &self.streamer_message
     }
 
+    /// Returns the block hash. It is a shortcut to get the data from the block header.
     pub fn block_hash(&self) -> CryptoHash {
         self.header().hash()
     }
 
+    /// Returns the previous block hash. It is a shortcut to get the data from the block header.
     pub fn prev_block_hash(&self) -> CryptoHash {
         self.header().prev_hash()
     }
 
+    /// Returns the block height. It is a shortcut to get the data from the block header.
     pub fn block_height(&self) -> u64 {
         self.header().height()
     }
 
+    /// Returns a [BlockHeader] structure of the block
+    ///
+    ///See [BlockHeader] structure sections for details.
     pub fn header(&self) -> BlockHeader {
         (&self.streamer_message).into()
     }
 
+    /// Returns an iterator over the [Receipt](crate::receipts::Receipt)s executed in this [Block].
+    ///
+    /// This field is a representation of `StreamerMessage.shard[N].receipt_execution_outcomes`
+    ///
+    /// A reminder that `receipt_execution_outcomes` has a type [near_indexer_primitives::IndexerExecutionOutcomeWithReceipt] which is an
+    /// ephemeral structure from `near-indexer-primitives` that hold a [near_primitives::views::ExecutionOutcomeView]
+    /// along with the corresponding [near_primitives::views::ReceiptView].
     pub fn receipts(&mut self) -> impl Iterator<Item = &receipts::Receipt> {
         if self.executed_receipts.is_empty() {
             self.executed_receipts = self
@@ -52,6 +88,10 @@ impl Block {
         self.executed_receipts.iter()
     }
 
+    /// Returns an iterator of [Receipt](crate::receipts::Receipt) included yet not executed in the [Block].
+    ///
+    /// [Receipts](crate::receipts::Receipt) included on the chain but not executed yet are called "postponed",
+    /// they are represented by the same structure [Receipt](crate::receipts::Receipt).
     pub fn postponed_receipts(&mut self) -> impl Iterator<Item = &receipts::Receipt> {
         if self.postponed_receipts.is_empty() {
             let executed_receipts_ids: Vec<_> = self
@@ -72,6 +112,12 @@ impl Block {
         self.postponed_receipts.iter()
     }
 
+    /// Returns an iterator of the [Transactions](crate::transactions::Transaction) included in the [Block].
+    ///
+    /// **Heads up!** Some indexer developers care about [Transaction](crate::transactions::Transaction)s for the knowledge where
+    /// the action chain has begun. Other indexer developers care about it because of the habits
+    /// from other blockchains like Ethereum where a transaction is a main asset. In case of NEAR
+    /// [Receipts](crate::receipts::Receipt) are more important.
     pub fn transactions(&mut self) -> impl Iterator<Item = &transactions::Transaction> {
         if self.transactions.is_empty() {
             self.transactions = self
@@ -87,6 +133,7 @@ impl Block {
         self.transactions.iter()
     }
 
+    /// Internal method to build the cache of actions on demand
     fn actions_from_streamer_message(&self) -> Vec<actions::Action> {
         self.streamer_message()
             .shards
@@ -99,6 +146,7 @@ impl Block {
             .collect()
     }
 
+    /// Returns an iterator of the [Actions](crate::actions::Action) executed in the [Block]
     pub fn actions(&mut self) -> impl Iterator<Item = &actions::Action> {
         if self.actions.is_empty() {
             self.build_actions_cache();
@@ -106,6 +154,7 @@ impl Block {
         self.actions.iter()
     }
 
+    /// Returns an iterator of the [Events](crate::events::Event) emitted in the [Block]
     pub fn events(&mut self) -> impl Iterator<Item = &events::Event> {
         if self.events.is_empty() {
             self.build_events_hashmap();
@@ -113,6 +162,7 @@ impl Block {
         self.events.values().flatten()
     }
 
+    /// Returns an iterator of the [StateChanges](crate::state_changes::StateChange) happened in the [Block]
     pub fn state_changes(&mut self) -> impl Iterator<Item = &state_changes::StateChange> {
         if self.state_changes.is_empty() {
             self.state_changes = self
@@ -126,6 +176,9 @@ impl Block {
         self.state_changes.iter()
     }
 
+    /// Helper to get all the [Actions](crate::actions::Action) by the single [Receipt](crate::receipts::Receipt)
+    ///
+    /// **Heads up!** This methods searches for the actions in the current [Block] only.
     pub fn actions_by_receipt_id<'a>(
         &'a mut self,
         receipt_id: &'a super::ReceiptId,
@@ -134,6 +187,7 @@ impl Block {
             .filter(move |action| &action.receipt_id() == receipt_id)
     }
 
+    /// Helper to get all the [Events](crate::events::Event) emitted by the specific [Receipt](crate::receipts::Receipt)
     pub fn events_by_receipt_id(&mut self, receipt_id: &super::ReceiptId) -> Vec<events::Event> {
         if self.events.is_empty() {
             self.build_events_hashmap();
@@ -145,6 +199,7 @@ impl Block {
         }
     }
 
+    /// Helper to get all the [Events](crate::events::Event) emitted by the specific contract ([AccountId](crate::near_indexer_primitives::types::AccountId))
     pub fn events_by_contract_id<'a>(
         &'a mut self,
         account_id: &'a crate::near_indexer_primitives::types::AccountId,
@@ -153,6 +208,7 @@ impl Block {
             .filter(move |event| event.is_emitted_by_contract(&account_id.clone()))
     }
 
+    /// Helper to get a specific [Receipt](crate::receipts::Receipt) by the [ReceiptId](crate::types::ReceiptId)
     pub fn receipt_by_id(&mut self, receipt_id: &super::ReceiptId) -> Option<&receipts::Receipt> {
         self.receipts()
             .find(|receipt| &receipt.receipt_id() == receipt_id)
@@ -160,10 +216,12 @@ impl Block {
 }
 
 impl Block {
+    // Internal method to build the cache of actions on demand
     fn build_actions_cache(&mut self) {
         self.actions = self.actions_from_streamer_message().to_vec();
     }
 
+    // Internal method to build the cache of events on demand
     fn build_events_hashmap(&mut self) {
         self.events = self
             .receipts()
@@ -186,6 +244,11 @@ impl From<StreamerMessage> for Block {
     }
 }
 
+/// Replacement for [`BlockHeaderView`](near_primitives::views::BlockHeaderView) from `near-primitives`. Shrank and simplified.
+/// We were trying to leave only the fields indexer developers might be interested in.
+///
+/// Friendly reminder, the original [`BlockHeaderView`](near_primitives::views::BlockHeaderView) is still accessible
+/// via [`.streamer_message()`](Block::streamer_message()) method.
 #[derive(Debug, Clone)]
 pub struct BlockHeader {
     height: u64,
@@ -204,54 +267,70 @@ pub struct BlockHeader {
 }
 
 impl BlockHeader {
+    /// The height of the [Block]
     pub fn height(&self) -> u64 {
         self.height
     }
 
+    /// The hash of the [Block]
     pub fn hash(&self) -> CryptoHash {
         self.hash
     }
 
+    /// The hash of the previous [Block]
     pub fn prev_hash(&self) -> CryptoHash {
         self.prev_hash
     }
 
+    /// The [AccountId](crate::near_indexer_primitives::types::AccountId) of the author of the [Block]
     pub fn author(&self) -> AccountId {
         self.author.clone()
     }
 
+    /// The timestamp of the [Block] in nanoseconds
     pub fn timestamp_nanosec(&self) -> u64 {
         self.timestamp_nanosec
     }
 
+    /// The [CryptoHash] of the epoch the [Block] belongs to
     pub fn epoch_id(&self) -> CryptoHash {
         self.epoch_id
     }
 
+    /// The [CryptoHash] of the next epoch
     pub fn next_epoch_id(&self) -> CryptoHash {
         self.next_epoch_id
     }
 
+    /// The gas price of the [Block]
     pub fn gas_price(&self) -> u128 {
         self.gas_price
     }
 
+    /// The total supply of the [Block]
     pub fn total_supply(&self) -> u128 {
         self.total_supply
     }
 
+    /// The latest protocol version of the [Block]
     pub fn latest_protocol_version(&self) -> u32 {
         self.latest_protocol_version
     }
 
+    /// The random value of the [Block]
     pub fn random_value(&self) -> CryptoHash {
         self.random_value
     }
 
+    /// The number of chunks included in the [Block]
     pub fn chunks_included(&self) -> u64 {
         self.chunks_included
     }
 
+    /// The validator proposals of the [Block]
+    ///
+    /// **Heads up!** This methods returns types defined in the `near-primitives` crate as is.
+    /// It is a subject of change in the future (once we define the corresponding Lake Primitives types)
     pub fn validator_proposals(&self) -> Vec<views::validator_stake_view::ValidatorStakeView> {
         self.validator_proposals.clone()
     }
--- lake-primitives/src/types/events.rs
+++ lake-primitives/src/types/events.rs
@@ -2,6 +2,8 @@ use crate::AccountId;
 
 use super::receipts::Receipt;
 
+/// Hight-level representation of the Event according to the [Events Format](https://nomicon.io/Standards/EventsFormat.html).
+/// In addition to the event this structure holds the data about the related [Receipt]: `receipt_id`, `receiver_id` and `predecessor_id`. All these fields are accessible via the corresponding getters.
 #[derive(Clone, Debug)]
 pub struct Event {
     pub(crate) related_receipt_id: crate::CryptoHash,
@@ -11,30 +13,40 @@ pub struct Event {
 }
 
 impl Event {
+    /// Returns the `event` value from the [RawEvent].
     pub fn event(&self) -> &str {
         &self.raw_event.event
     }
 
+    /// Returns the `standard` value from the [RawEvent].
     pub fn standard(&self) -> &str {
         &self.raw_event.standard
     }
 
+    /// Returns the `version` value from the [RawEvent].
     pub fn version(&self) -> &str {
         &self.raw_event.version
     }
 
+    /// Returns the `data` value from the [RawEvent] if present, otherwise returns `None`.
     pub fn data(&self) -> Option<&serde_json::Value> {
         self.raw_event.data.as_ref()
     }
 
+    /// Returns the [CryptoHash](crate::CryptoHash) id of the related [Receipt].
+    ///
+    /// **Please note** that events are emitted through the `ExecutionOutcome` logs. In turn, the `ExecutionOutcome`
+    /// is a result of the execution of the [Receipt].
     pub fn related_receipt_id(&self) -> crate::CryptoHash {
         self.related_receipt_id
     }
 
+    /// Returns the [AccountId] of the receiver of the related [Receipt].
     pub fn related_receipt_receiver_id(&self) -> &AccountId {
         &self.receiver_id
     }
 
+    /// Returns the [AccountId] of the predecessor of the related [Receipt].
     pub fn related_receipt_predecessor_id(&self) -> &AccountId {
         &self.predecessor_id
     }
@@ -45,6 +57,8 @@ impl Event {
     }
 }
 
+/// This structure is an honest representation of the Events Format standard described here
+/// <https://nomicon.io/Standards/EventsFormat>
 #[derive(Clone, Debug, serde::Deserialize)]
 pub struct RawEvent {
     pub event: String,
@@ -54,6 +68,7 @@ pub struct RawEvent {
 }
 
 impl RawEvent {
+    /// Parses the log message (originated from `ExecutionOutcome` but not limited) and returns the RawEvent.
     pub fn from_log(log: &str) -> anyhow::Result<Self> {
         let prefix = "EVENT_JSON:";
         if !log.starts_with(prefix) {
@@ -71,6 +86,7 @@ pub trait EventsTrait<Receipt> {
 }
 
 impl EventsTrait<Receipt> for Receipt {
+    /// Reads the logs from the [Receipt] and extracts all the [Events](Event) from it into a Vec.
     fn events(&self) -> Vec<Event> {
         self.logs()
             .iter()
